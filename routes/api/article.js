b035d1a25b81ae5dded82d1f355a39e7036c9faf
Olga <oserenchenko@gmail.com>
2018-10-08 21:15:48 -0600

    models, controller, and routes set up

---
+++ routes/api/article.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const articlesController = require("../../controllers/articlesController");
+
+// Matches with "/api/books"
+router.route("/")
+  .get(articlesController.findAll)
+  .post(articlesController.create);
+
+// Matches with "/api/books/:id"
+router.route("/:id")
+  .delete(articlesController.remove);
+
+//main html page
+router.route("/*")
+  .load("../../client/public/index.html")
+
+module.exports = router;
